a155d15e6356766eb040680831eb7c8fffad3160
a114413 <a114413@alunos.uminho.pt>
2025-12-07 10:35:28 +0000

    4=10

--- main.hs
+++ main.hs
@@ -1,18 +1,54 @@
 module Main where 
 
 data Operation = Suma | Resta | Mult | Div 
-    deriving (Eq, Show) 
+    deriving (Eq) 
 
 data Expr = Val Double | Op Operation Expr Expr 
-    deriving (Eq, Show) 
+    deriving (Eq)
+
+instance Show Operation where 
+    show Suma = "+"
+    show Mult = "*"
+    show Div = "/"
+    show Resta = "-"
+
+instance Show Expr where
+    show (Val n) = 
+        let entero = round n
+        in if n == fromIntegral entero then show entero else show n 
+    show (Op op izq der) = "(" ++ show izq ++ " " ++ show op ++ " " ++ show der ++ ")"  
 
 main :: IO ()
 main = do
-    let prueba = Op Mult (Op Suma (Val 8) (Val 2)) (Val 4)
-    putStrLn "Expresión:"
-    print prueba
-    putStrLn "Resultado:"
-    print (eval prueba)
+    putStrLn "BIENVENIDO a 4=10"
+    jugar
+   
+
+jugar :: IO () 
+jugar = do 
+    putStrLn "\n----------------"
+    putStrLn "Introduce 4 números separados por espacios"
+    input <- getLine
+    let numeros = leerNumeros input 
+    putStrLn ("Buscando soluciones para:" ++ show numeros) 
+    let resultados = soluciones numeros 
+    if null resultados 
+        then putStrLn "No se encontró ninguna solución" 
+        else do 
+            putStrLn "¡Éxito! Aquí tienes una forma de resolverlo:"
+            print (head resultados) 
+    putStrLn "------------------------------"
+    putStrLn "Quiere jugar de nuevo? (s/n)" 
+    respuesta <- getLine 
+    if respuesta == "s" || respuesta == "S"
+        then jugar 
+        else putStrLn "Gracias por jugar!" 
+
+
+
+leerNumeros :: String -> [Expr]
+-- "8 4 2 6" = lista de expr 
+leerNumeros texto = map Val (map read (words texto))
 
 aplicarOp :: Operation -> Double -> Double -> Maybe Double 
 aplicarOp Suma n1 n2 = Just (n1 + n2) 
@@ -27,9 +63,18 @@ eval (Op op izq dir) =
         (Just n1, Just n2) -> aplicarOp op n1 n2 
         _ -> Nothing 
 
+--Función combinatoria 
 sacarUna :: [a] -> [(a, [a])]
 sacarUna [] = [] 
-sacarUna (x:xs) = (x:xs):[(y, x:resto) | (y, resto) <- sacarUna xs]
+sacarUna (x:xs) = (x, xs):[(y, x:resto) | (y, resto) <- sacarUna xs]
 
 sacarDos :: [a] -> [(a, a, [a])]
-sacarDos _ = undefined 
+sacarDos lista = [(x, y, restoFinal) | (x, resto1) <- sacarUna lista, (y, restoFinal) <- sacarUna resto1]
+
+soluciones :: [Expr] -> [Expr] 
+soluciones [] = []
+soluciones [e] = 
+    case eval e of 
+        Just resultado -> if abs (resultado - 10.0) < 0.001 then [e] else [] 
+        Nothing -> []
+soluciones lista = [sol | (a, b, resto) <- sacarDos lista, op <- [Suma, Resta, Mult, Div], let nuevaExpr = Op op a b, sol <- soluciones (nuevaExpr : resto)]
